8772e133e3e53ec4459fbe996953cbb795586ffa
Gopher8484 <129989075+Gopher8484@users.noreply.github.com>
2023-04-07 09:32:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -15,28 +15,36 @@ def get_token():
         'redirect_uri':'http://localhost:53682/'
     }
 
-    r = requests.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
+    r = requests.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', data=data, headers=headers)
     token_json = r.json()
     access_token = token_json['access_token']
 
     return access_token
 
 def main(access_token):
+    endpoints = [
+        r'https://graph.microsoft.com/v1.0/me/drive/root',
+        r'https://graph.microsoft.com/v1.0/me/drive',
+        r'https://graph.microsoft.com/v1.0/drive/root',
+        r'https://graph.microsoft.com/v1.0/users',
+        r'https://graph.microsoft.com/v1.0/me/messages',
+        r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+        r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',
+        r'https://graph.microsoft.com/v1.0/me/drive/root/children',
+        r'https://api.powerbi.com/v1.0/myorg/apps',
+        r'https://graph.microsoft.com/v1.0/me/mailFolders',
+        r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories'
+    ]
+
     headers={
     'Authorization':access_token,
     'Content-Type':'application/json'
     }
-    requests.get(r'https://graph.microsoft.com/v1.0/me/drive/root', headers=headers)
-    requests.get(r'https://graph.microsoft.com/v1.0/me/drive', headers=headers)
-    requests.get(r'https://graph.microsoft.com/v1.0/drive/root', headers=headers)
-    requests.get(r'https://graph.microsoft.com/v1.0/users', headers=headers)
-    requests.get(r'https://graph.microsoft.com/v1.0/me/messages', headers=headers)    
-    requests.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules', headers=headers)    
-    requests.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta', headers=headers)
-    requests.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children', headers=headers)
-    requests.get(r'https://api.powerbi.com/v1.0/myorg/apps', headers=headers) 
-    requests.get(r'https://graph.microsoft.com/v1.0/me/mailFolders', headers=headers)
-    requests.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories', headers=headers)
+
+    for endpoint in endpoints:
+        r = requests.get(endpoint, headers=headers)
+        print(r.status_code)
+        r.raise_for_status()
 
 token = get_token()
 
